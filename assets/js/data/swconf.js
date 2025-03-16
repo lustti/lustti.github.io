096b0d5c36aa7701bf2668dbac7438dfaee89576
worklt <worklt@163.com>
2025-03-16 18:31:38 +0800

    chore: change back to english

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742120720',resources: [
+    cacheName: 'chirpy-1742121041',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
