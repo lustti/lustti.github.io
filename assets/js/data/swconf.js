9ac26a2b69dbbf23598f598f6ae083170f4b7823
worklt <worklt@163.com>
2025-03-16 13:59:05 +0800

    pref: update config infos

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742103638',resources: [
+    cacheName: 'chirpy-1742104677',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
