6560fb30de0cb2b5e2c9a2d7d58ff6054d1bc4d7
worklt <worklt@163.com>
2025-03-16 19:00:58 +0800

    docs: fix tiny error

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742122120',resources: [
+    cacheName: 'chirpy-1742122825',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
