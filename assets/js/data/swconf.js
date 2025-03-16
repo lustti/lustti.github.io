9c0ec263dd11f3afdc4e52b82bac2e01e196650f
worklt <worklt@163.com>
2025-03-16 18:27:11 +0800

    fix: local debug build error to release

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742116506',resources: [
+    cacheName: 'chirpy-1742120720',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
