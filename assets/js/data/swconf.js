49a9abf0ae1e8137103801809934e7a5c6de7e7b
worklt <worklt@163.com>
2025-03-16 17:27:23 +0800

    feat: first working version

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742105971',resources: [
+    cacheName: 'chirpy-1742116506',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
