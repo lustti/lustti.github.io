f22538f5fdaa7c55395546f190a899571b25603c
worklt <worklt@163.com>
2025-03-18 11:43:13 +0800

    docs: open mermaid in about

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742269137',resources: [
+    cacheName: 'chirpy-1742269371',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
