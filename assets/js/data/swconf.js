bdd791df0c2cc8e666621e45a8656b7010241731
worklt <worklt@163.com>
2025-03-19 17:11:07 +0800

    docs: add post centos cuda install

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742374113',resources: [
+    cacheName: 'chirpy-1742375235',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
