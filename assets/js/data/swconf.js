36be67728c484938cbf92056a330526ae98dbd42
worklt <worklt@163.com>
2025-03-21 08:59:33 +0800

    docs: update category and tags, and add ffi error solution

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742378480',resources: [
+    cacheName: 'chirpy-1742518577',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
